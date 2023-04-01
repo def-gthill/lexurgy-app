e930b2a1cd49cca93396ed3a785b6f96c8d36785
Graham Hill <gthill97@gmail.com>
2023-04-01 14:12:33 -0600

    Set up styling and remove template junk

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,30 @@
 import { signOut } from "next-auth/react";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import styles from "@/styles/Header.module.css";
 
 export default function Header() {
   return (
-    <button
-      onClick={() => {
-        signOut();
-      }}
-    >
-      Sign Out
-    </button>
+    <NavigationMenu.Root>
+      <NavigationMenu.List className={styles.NavigationMenuList}>
+        <NavigationMenu.Item>
+          <NavigationMenu.Link
+            className={styles.NavigationMenuLink}
+            href="https://github.com/def-gthill/lexurgy-app"
+          >
+            GitHub
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+          <NavigationMenu.Link
+            className={styles.NavigationMenuLink}
+            onSelect={() => {
+              signOut();
+            }}
+          >
+            Sign Out
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+      </NavigationMenu.List>
+    </NavigationMenu.Root>
   );
 }
