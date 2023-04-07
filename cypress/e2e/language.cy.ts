9f0a3441bc6b808baa57e3811342df63118d4928
Graham Hill <gthill97@gmail.com>
2023-04-07 14:13:57 -0600

    Don't abbreviate language

--- cypress/e2e/language.cy.ts
+++ cypress/e2e/language.cy.ts
@@ -5,19 +5,15 @@ describe("a language page", () => {
     cy.exec("npm run db:reset && npm run db:seed:examplish");
   });
 
-  it("has the language name in its title", () => {
-    cy.visit(`/lang/${examplishUuid}`);
-    cy.title().should("equal", "Lexurgy - Examplish");
-  });
-
   it("displays the language name", () => {
-    cy.visit(`/lang/${examplishUuid}`);
+    cy.visit(`/language/${examplishUuid}`);
+    cy.title().should("equal", "Lexurgy - Examplish");
     cy.contains("Examplish").should("be.visible");
   });
 
   describe("the translation editor", () => {
     it("lets the user create a simple translation", () => {
-      cy.visit(`/lang/${examplishUuid}`);
+      cy.visit(`/language/${examplishUuid}`);
       cy.contains("Add Translation").click();
       cy.get("#text").type("Sha dor.");
       cy.get("#translation").type("The cat is sleeping.");
