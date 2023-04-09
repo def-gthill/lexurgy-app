4c5e2cd9410785a2a580e190e43bdc5284aab782
Graham Hill <gthill97@gmail.com>
2023-04-09 15:49:54 -0600

    Use Language Info on language page

--- src/pages/language/[id].tsx
+++ src/pages/language/[id].tsx
@@ -1,3 +1,4 @@
+import LanguageInfo from "@/components/LanguageInfo";
 import Language from "@/models/Language";
 import Translation from "@/models/Translation";
 import * as Label from "@radix-ui/react-label";
@@ -17,10 +18,6 @@ export default function LanguageOverview() {
     useState("");
   const router = useRouter();
   const id = router.query.id as string;
-  const { data: language, error } = useSWR<Language, Error>(
-    `/api/languages/${id}`,
-    fetcher
-  );
   const { data } = useSWR<Translation[], Error>(
     `/api/translations?language=${id}`,
     fetcher
@@ -28,67 +25,68 @@ export default function LanguageOverview() {
   const translations = data || [];
   const { mutate } = useSWRConfig();
 
-  if (error !== undefined) {
-    return <div>Language not found</div>;
-  } else if (language === undefined) {
-    return <div>Loading language...</div>;
-  }
   return (
-    <>
-      <Head>
-        <title>Lexurgy - {language.name}</title>
-        <meta
-          name="description"
-          content={`"${language.name}, a constructed language"`}
-        />
-      </Head>
-      <main>
-        <h1>{language.name}</h1>
-        <h2>Translations</h2>
-        <button onClick={() => setShowingTranslationEditor(true)}>
-          Add Translation
-        </button>
-        {showingTranslationEditor && (
-          <>
-            <Label.Root htmlFor="text">{language.name} Text</Label.Root>
-            <input
-              type="text"
-              id="text"
-              onChange={(event) => setTranslationEditorText(event.target.value)}
-              value={translationEditorText}
-            ></input>
-            <Label.Root htmlFor="translation">Free Translation</Label.Root>
-            <input
-              type="text"
-              id="translation"
-              onChange={(event) =>
-                setTranslationEditorTranslation(event.target.value)
-              }
-              value={translationEditorTranslation}
-            ></input>
-            <button
-              onClick={() =>
-                saveTranslation({
-                  languageId: id,
-                  romanized: translationEditorText,
-                  translation: translationEditorTranslation,
-                })
-              }
-            >
-              Save
+    <LanguageInfo
+      content={(language: Language) => (
+        <>
+          <Head>
+            <title>Lexurgy - {language.name}</title>
+            <meta
+              name="description"
+              content={`"${language.name}, a constructed language"`}
+            />
+          </Head>
+          <main>
+            <h1>{language.name}</h1>
+            <h2>Translations</h2>
+            <button onClick={() => setShowingTranslationEditor(true)}>
+              Add Translation
             </button>
-          </>
-        )}
-        {translations.map((translation) => (
-          <>
-            <p>
-              <i>{translation.romanized}</i>
-            </p>
-            <p>{`"${translation.translation}"`}</p>
-          </>
-        ))}
-      </main>
-    </>
+            {showingTranslationEditor && (
+              <>
+                <Label.Root htmlFor="text">{language.name} Text</Label.Root>
+                <input
+                  type="text"
+                  id="text"
+                  onChange={(event) =>
+                    setTranslationEditorText(event.target.value)
+                  }
+                  value={translationEditorText}
+                ></input>
+                <Label.Root htmlFor="translation">Free Translation</Label.Root>
+                <input
+                  type="text"
+                  id="translation"
+                  onChange={(event) =>
+                    setTranslationEditorTranslation(event.target.value)
+                  }
+                  value={translationEditorTranslation}
+                ></input>
+                <button
+                  onClick={() =>
+                    saveTranslation({
+                      languageId: id,
+                      romanized: translationEditorText,
+                      translation: translationEditorTranslation,
+                    })
+                  }
+                >
+                  Save
+                </button>
+              </>
+            )}
+            {translations.map((translation) => (
+              <>
+                <p>
+                  <i>{translation.romanized}</i>
+                </p>
+                <p>{`"${translation.translation}"`}</p>
+              </>
+            ))}
+          </main>
+        </>
+      )}
+    />
   );
 
   async function saveTranslation(translation: Translation) {
