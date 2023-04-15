0b8af2acc5d1ed845d51ab7885e74fbe00c63f0f
Graham Hill <gthill97@gmail.com>
2023-04-15 14:33:12 -0600

    Fix hydration error on header link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@ import styles from "@/styles/Header.module.css";
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import { signOut } from "next-auth/react";
 import NextLink from "next/link";
-import { useRouter } from "next/router";
 
 export default function Header() {
   return (
@@ -10,6 +9,9 @@ export default function Header() {
       <NavigationMenu.List className={styles.NavigationMenuList}>
         <NavigationMenu.Item>
           <Link href="/">Lexurgy</Link>
+          {/* <NavigationMenu.Link className={styles.NavigationMenuLink} href="/">
+            Lexurgy
+          </NavigationMenu.Link> */}
         </NavigationMenu.Item>
         <NavigationMenu.Item>
           <NavigationMenu.Link
@@ -35,16 +37,9 @@ export default function Header() {
 }
 
 function Link({ href, ...props }: { href: string; [index: string]: unknown }) {
-  const router = useRouter();
-  const isActive = router.asPath === href;
-
   return (
-    <NextLink href={href} passHref>
-      <NavigationMenu.Link
-        className={styles.NavigationMenuLink}
-        active={isActive}
-        {...props}
-      />
+    <NextLink href={href} passHref legacyBehavior>
+      <NavigationMenu.Link className={styles.NavigationMenuLink} {...props} />
     </NextLink>
   );
 }
