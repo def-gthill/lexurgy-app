16ececa926ccadb5c87fda1993a8f6efb34bff53
Graham Hill <gthill97@gmail.com>
2023-04-21 15:41:14 -0600

    Create collection endpoint wrapper

---
+++ src/api.ts
@@ -0,0 +1,19 @@
+import { HttpStatusCode } from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+type RequestQuery = NextApiRequest["query"];
+
+export async function collectionEndpoint<T>(
+  req: NextApiRequest,
+  res: NextApiResponse<T | T[]>,
+  get: (query: RequestQuery) => Promise<T[]>,
+  post: (resource: T) => Promise<T>
+) {
+  if (req.method == "GET") {
+    res.status(HttpStatusCode.Ok).json(await get(req.query));
+  } else if (req.method == "POST") {
+    res.status(HttpStatusCode.Created).json(await post(req.body));
+  } else {
+    res.status(HttpStatusCode.MethodNotAllowed);
+  }
+}
--- src/pages/api/languages.ts
+++ src/pages/api/languages.ts
@@ -1,3 +1,4 @@
+import { collectionEndpoint } from "@/api";
 import getDriver, { execute, query } from "@/db";
 import Language from "@/models/Language";
 import * as crypto from "crypto";
@@ -5,24 +6,23 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const driver = getDriver();
 
-export default async function handler(
+export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Language | Language[]>
 ) {
-  if (req.method === "GET") {
-    const result = await query<Language>(
-      driver,
-      "MATCH (lang:Language) RETURN lang;"
-    );
-    res.status(200).json(result);
-  } else if (req.method === "POST") {
-    const language = req.body as Language;
-    if (language.id === undefined) {
-      language.id = crypto.randomUUID();
-    }
-    await execute(driver, "CREATE (:Language {id: $id, name: $name})", {
-      ...language,
-    });
-    res.status(201).json(language);
+  collectionEndpoint(req, res, getLanguages, postLanguage);
+}
+
+async function getLanguages(): Promise<Language[]> {
+  return await query<Language>(driver, "MATCH (lang:Language) RETURN lang;");
+}
+
+async function postLanguage(language: Language): Promise<Language> {
+  if (language.id === undefined) {
+    language.id = crypto.randomUUID();
   }
+  await execute(driver, "CREATE (:Language {id: $id, name: $name})", {
+    ...language,
+  });
+  return language;
 }
