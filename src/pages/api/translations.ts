6cb5caacf2cb7311d6593300344c13dd5739693a
Graham Hill <gthill97@gmail.com>
2023-04-21 16:24:03 -0600

    Use the endpoint wrapper for translations

--- src/pages/api/translations.ts
+++ src/pages/api/translations.ts
@@ -1,3 +1,4 @@
+import { RequestQuery, collectionEndpoint } from "@/api";
 import getDriver, { execute, query } from "@/db";
 import Construction from "@/models/Construction";
 import Lexeme from "@/models/Lexeme";
@@ -26,73 +27,43 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Translation | Translation[]>
 ) {
-  if (req.method === "GET") {
-    const languageId = req.query.language as string;
-    const result = (
-      await query<QueryTranslation>(
-        driver,
-        `MATCH (tr:Translation) -[:IS_IN]-> (lang:Language {id: $id})
-        OPTIONAL MATCH (tr) -[:HAS_STRUCTURE]-> (node:SyntaxNode)
-        OPTIONAL MATCH (node) -[:HAS_TYPE]-> (cons:Construction)
-        OPTIONAL MATCH (node) -[child:HAS_CHILD]-> (word:Word)
-        OPTIONAL MATCH (word) -[:HAS_STEM]-> (lex:Lexeme)
-        WITH tr, node, cons, collect([child.name, word {romanized: coalesce(lex.romanized, word.romanized), .stemId }]) as children
-        RETURN tr {.id, .romanized, structure: node {nodeTypeId: cons.id, construction: cons {.*}, children: children}, .translation}`,
-        { id: languageId }
-      )
-    ).map((translation) => {
-      if (translation.structure) {
-        const structure = convertStructure(translation.structure);
-        return {
-          ...translation,
-          languageId,
-          romanized: structureToRomanized(structure),
-          structure,
-        };
-      } else {
-        return {
-          ...translation,
-          languageId,
-          structure: undefined,
-        };
-      }
-    });
-    res.status(200).json(result);
-  } else if (req.method === "POST") {
-    const translation = req.body as Translation;
-    if (translation.id === undefined) {
-      translation.id = crypto.randomUUID();
-    }
-    if (translation.languageId && translation.structure) {
-      translation.structure = await linkStems(
-        translation.languageId,
-        translation.structure
-      );
-    }
+  await collectionEndpoint(req, res, getTranslations, postTranslation);
+}
+
+async function getTranslations(
+  requestQuery: RequestQuery
+): Promise<Translation[]> {
+  const languageId = requestQuery.language as string;
+  const result = (
+    await query<QueryTranslation>(
+      driver,
+      `MATCH (tr:Translation) -[:IS_IN]-> (lang:Language {id: $id})
+      OPTIONAL MATCH (tr) -[:HAS_STRUCTURE]-> (node:SyntaxNode)
+      OPTIONAL MATCH (node) -[:HAS_TYPE]-> (cons:Construction)
+      OPTIONAL MATCH (node) -[child:HAS_CHILD]-> (word:Word)
+      OPTIONAL MATCH (word) -[:HAS_STEM]-> (lex:Lexeme)
+      WITH tr, node, cons, collect([child.name, word {romanized: coalesce(lex.romanized, word.romanized), .stemId }]) as children
+      RETURN tr {.id, .romanized, structure: node {nodeTypeId: cons.id, construction: cons {.*}, children: children}, .translation}`,
+      { id: languageId }
+    )
+  ).map((translation) => {
     if (translation.structure) {
-      translation.romanized = structureToRomanized(translation.structure);
+      const structure = convertStructure(translation.structure);
+      return {
+        ...translation,
+        languageId,
+        romanized: structureToRomanized(structure),
+        structure,
+      };
+    } else {
+      return {
+        ...translation,
+        languageId,
+        structure: undefined,
+      };
     }
-    let query = `
-    MATCH (lang:Language {id: $languageId})
-    CREATE (tr:Translation {id: $id, romanized: $romanized, translation: $translation})
-    CREATE (tr) -[:IS_IN]-> (lang)`;
-    if (translation.structure) {
-      query += `
-      CREATE (tr) -[:HAS_STRUCTURE]-> (node:SyntaxNode)
-      WITH node
-      MATCH (cons:Construction {id: $structure.nodeTypeId})
-      CREATE (node) -[:HAS_TYPE]-> (cons)
-      WITH node
-      UNWIND [k in KEYS($structure.children) | [k, $structure.children[k]]] AS child
-      CREATE (word:Word {romanized: child[1].romanized})
-      CREATE (node) -[:HAS_CHILD {name: child[0]}]-> (word)
-      WITH word, child[1].stemId AS stemId
-      MATCH (lex:Lexeme {id: stemId})
-      CREATE (word) -[:HAS_STEM]-> (lex)`;
-    }
-    await execute(driver, query, { ...translation });
-    res.status(201).json(translation);
-  }
+  });
+  return result;
 }
 
 function convertStructure(structure: QuerySyntaxNode): SyntaxNode {
@@ -105,6 +76,41 @@ function convertStructure(structure: QuerySyntaxNode): SyntaxNode {
       ])
     ),
   };
+}
+
+async function postTranslation(translation: Translation): Promise<Translation> {
+  if (translation.id === undefined) {
+    translation.id = crypto.randomUUID();
+  }
+  if (translation.languageId && translation.structure) {
+    translation.structure = await linkStems(
+      translation.languageId,
+      translation.structure
+    );
+  }
+  if (translation.structure) {
+    translation.romanized = structureToRomanized(translation.structure);
+  }
+  let query = `
+  MATCH (lang:Language {id: $languageId})
+  CREATE (tr:Translation {id: $id, romanized: $romanized, translation: $translation})
+  CREATE (tr) -[:IS_IN]-> (lang)`;
+  if (translation.structure) {
+    query += `
+    CREATE (tr) -[:HAS_STRUCTURE]-> (node:SyntaxNode)
+    WITH node
+    MATCH (cons:Construction {id: $structure.nodeTypeId})
+    CREATE (node) -[:HAS_TYPE]-> (cons)
+    WITH node
+    UNWIND [k in KEYS($structure.children) | [k, $structure.children[k]]] AS child
+    CREATE (word:Word {romanized: child[1].romanized})
+    CREATE (node) -[:HAS_CHILD {name: child[0]}]-> (word)
+    WITH word, child[1].stemId AS stemId
+    MATCH (lex:Lexeme {id: stemId})
+    CREATE (word) -[:HAS_STEM]-> (lex)`;
+  }
+  await execute(driver, query, { ...translation });
+  return translation;
 }
 
 async function linkStems(
