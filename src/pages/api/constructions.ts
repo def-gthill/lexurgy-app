5eb35fd145cf2f7629c6014e3af349607d2321e5
Graham Hill <gthill97@gmail.com>
2023-04-21 16:15:35 -0600

    Use the endpoint wrapper for constructions

--- src/pages/api/constructions.ts
+++ src/pages/api/constructions.ts
@@ -1,22 +1,44 @@
-import getDriver, { query } from "@/db";
+import { RequestQuery, collectionEndpoint } from "@/api";
+import getDriver, { execute, query } from "@/db";
 import Construction from "@/models/Construction";
+import * as crypto from "crypto";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const driver = getDriver();
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Construction[]>
+  res: NextApiResponse<Construction | Construction[]>
 ) {
-  if (req.method === "GET") {
-    const languageId = req.query.language as string;
-    const result = (
-      await query<Construction>(
-        driver,
-        "MATCH (cons:Construction) -[:IS_IN]-> (lang:Language {id: $id}) RETURN cons;",
-        { id: languageId }
-      )
-    ).map((construction) => ({ ...construction, languageId }));
-    res.status(200).json(result);
+  await collectionEndpoint(req, res, getConstructions, postConstruction);
+}
+
+async function getConstructions(
+  requestQuery: RequestQuery
+): Promise<Construction[]> {
+  const languageId = requestQuery.language as string;
+  const result = (
+    await query<Construction>(
+      driver,
+      "MATCH (cons:Construction) -[:IS_IN]-> (lang:Language {id: $id}) RETURN cons;",
+      { id: languageId }
+    )
+  ).map((construction) => ({ ...construction, languageId }));
+  return result;
+}
+
+async function postConstruction(
+  construction: Construction
+): Promise<Construction> {
+  if (construction.id === undefined) {
+    construction.id = crypto.randomUUID();
   }
+  await execute(
+    driver,
+    `MATCH (lang:Language {id: $languageId})
+    MERGE (cons:Construction {id: $id}) -[:IS_IN]-> (lang)
+    SET name = $name, children = $children`,
+    { ...construction }
+  );
+  return construction;
 }
