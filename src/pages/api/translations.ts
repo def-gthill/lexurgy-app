1124e1fdbda6bb7070a7427d474da09621c615dd
Graham Hill <gthill97@gmail.com>
2023-04-21 12:30:49 -0600

    Use the Neo4j wrapper for translations

--- src/pages/api/translations.ts
+++ src/pages/api/translations.ts
@@ -1,11 +1,26 @@
-import getDriver from "@/db";
+import getDriver, { execute, query } from "@/db";
+import Construction from "@/models/Construction";
 import Lexeme from "@/models/Lexeme";
 import SyntaxNode, { structureToRomanized } from "@/models/SyntaxNode";
 import Translation from "@/models/Translation";
+import Word from "@/models/Word";
 import * as crypto from "crypto";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const driver = getDriver();
+
+interface QueryTranslation {
+  id?: string;
+  romanized: string;
+  structure?: QuerySyntaxNode;
+  translation: string;
+}
+
+interface QuerySyntaxNode {
+  nodeTypeId?: string;
+  construction?: Construction;
+  children: [string, Word | QuerySyntaxNode][];
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,9 +28,9 @@ export default async function handler(
 ) {
   if (req.method === "GET") {
     const languageId = req.query.language as string;
-    const session = driver.session();
-    try {
-      const result = await session.run(
+    const result = (
+      await query<QueryTranslation>(
+        driver,
         `MATCH (tr:Translation) -[:IS_IN]-> (lang:Language {id: $id})
         OPTIONAL MATCH (tr) -[:HAS_STRUCTURE]-> (node:SyntaxNode)
         OPTIONAL MATCH (node) -[:HAS_TYPE]-> (cons:Construction)
@@ -24,35 +39,26 @@ export default async function handler(
         WITH tr, node, cons, collect([child.name, word {romanized: coalesce(lex.romanized, word.romanized), .stemId }]) as children
         RETURN tr {.id, .romanized, structure: node {nodeTypeId: cons.id, construction: cons {.*}, children: children}, .translation}`,
         { id: languageId }
-      );
-      const translations: Translation[] = result.records.map((record) => {
-        const properties = record.get("tr");
-        const result = {
-          id: properties.id as string,
+      )
+    ).map((translation) => {
+      if (translation.structure) {
+        const structure = convertStructure(translation.structure);
+        return {
+          ...translation,
           languageId,
-          romanized: properties.romanized as string,
-          translation: properties.translation as string,
+          romanized: structureToRomanized(structure),
+          structure,
         };
-        if (properties.structure) {
-          const structure = {
-            ...properties.structure,
-            children: Object.fromEntries(properties.structure.children),
-          };
-          return {
-            ...result,
-            romanized: structureToRomanized(structure),
-            structure,
-          };
-        } else {
-          return result;
-        }
-      });
-      res.status(200).json(translations);
-    } finally {
-      await session.close();
-    }
+      } else {
+        return {
+          ...translation,
+          languageId,
+          structure: undefined,
+        };
+      }
+    });
+    res.status(200).json(result);
   } else if (req.method === "POST") {
-    const session = driver.session();
     const translation = req.body as Translation;
     if (translation.id === undefined) {
       translation.id = crypto.randomUUID();
@@ -66,31 +72,39 @@ export default async function handler(
     if (translation.structure) {
       translation.romanized = structureToRomanized(translation.structure);
     }
-    try {
-      let query = `
-      MATCH (lang:Language {id: $languageId})
-      CREATE (tr:Translation {id: $id, romanized: $romanized, translation: $translation})
-      CREATE (tr) -[:IS_IN]-> (lang)`;
-      if (translation.structure) {
-        query += `
-        CREATE (tr) -[:HAS_STRUCTURE]-> (node:SyntaxNode)
-        WITH node
-        MATCH (cons:Construction {id: $structure.nodeTypeId})
-        CREATE (node) -[:HAS_TYPE]-> (cons)
-        WITH node
-        UNWIND [k in KEYS($structure.children) | [k, $structure.children[k]]] AS child
-        CREATE (word:Word {romanized: child[1].romanized})
-        CREATE (node) -[:HAS_CHILD {name: child[0]}]-> (word)
-        WITH word, child[1].stemId AS stemId
-        MATCH (lex:Lexeme {id: stemId})
-        CREATE (word) -[:HAS_STEM]-> (lex)`;
-      }
-      await session.run(query, translation);
-      res.status(201).json(translation);
-    } finally {
-      await session.close();
+    let query = `
+    MATCH (lang:Language {id: $languageId})
+    CREATE (tr:Translation {id: $id, romanized: $romanized, translation: $translation})
+    CREATE (tr) -[:IS_IN]-> (lang)`;
+    if (translation.structure) {
+      query += `
+      CREATE (tr) -[:HAS_STRUCTURE]-> (node:SyntaxNode)
+      WITH node
+      MATCH (cons:Construction {id: $structure.nodeTypeId})
+      CREATE (node) -[:HAS_TYPE]-> (cons)
+      WITH node
+      UNWIND [k in KEYS($structure.children) | [k, $structure.children[k]]] AS child
+      CREATE (word:Word {romanized: child[1].romanized})
+      CREATE (node) -[:HAS_CHILD {name: child[0]}]-> (word)
+      WITH word, child[1].stemId AS stemId
+      MATCH (lex:Lexeme {id: stemId})
+      CREATE (word) -[:HAS_STEM]-> (lex)`;
     }
+    await execute(driver, query, { ...translation });
+    res.status(201).json(translation);
   }
+}
+
+function convertStructure(structure: QuerySyntaxNode): SyntaxNode {
+  return {
+    ...structure,
+    children: Object.fromEntries(
+      structure.children.map(([childName, child]) => [
+        childName,
+        "romanized" in child ? child : convertStructure(child),
+      ])
+    ),
+  };
 }
 
 async function linkStems(
