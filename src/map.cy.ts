61af1798397c6a4083904684589940b88a2ce38c
Graham Hill <gthill97@gmail.com>
2023-04-21 22:12:40 -0600

    Add a pure unit test

---
+++ src/map.cy.ts
@@ -0,0 +1,37 @@
+import { update } from "./map";
+
+describe("The update function", () => {
+  it("can add an entry to an empty map", () => {
+    const result = update([], ["foo", 42]);
+    expect(result).to.deep.equal([["foo", 42]]);
+  });
+
+  it("can add an entry to a map with stuff in it", () => {
+    const result = update(
+      [
+        ["foo", 42],
+        ["bar", 97],
+      ],
+      ["baz", 216]
+    );
+    expect(result).to.deep.equal([
+      ["foo", 42],
+      ["bar", 97],
+      ["baz", 216],
+    ]);
+  });
+
+  it("replaces an existing entry with a matching key", () => {
+    const result = update(
+      [
+        ["foo", 42],
+        ["bar", 97],
+      ],
+      ["foo", 216]
+    );
+    expect(result).to.deep.equal([
+      ["foo", 216],
+      ["bar", 97],
+    ]);
+  });
+});
