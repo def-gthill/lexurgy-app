dab3de6442f2805c23d353736d08df40e176b76b
Graham Hill <gthill97@gmail.com>
2023-04-23 15:26:16 -0600

    Create a generic resource endpoint

--- src/api.ts
+++ src/api.ts
@@ -17,3 +17,31 @@ export async function collectionEndpoint<T>(
     res.status(HttpStatusCode.MethodNotAllowed);
   }
 }
+
+export async function resourceEndpoint<T>(
+  req: NextApiRequest,
+  res: NextApiResponse<T | string>,
+  get: (id: string) => Promise<T[]>,
+  delete_: (id: string) => Promise<T[]>
+) {
+  const { id } = req.query;
+  if (typeof id !== "string") {
+    res.status(HttpStatusCode.BadRequest);
+  } else if (req.method == "GET") {
+    const result = await get(id);
+    if (result.length === 0) {
+      res.status(HttpStatusCode.NotFound);
+    } else {
+      res.status(HttpStatusCode.Ok).json(result[0]);
+    }
+  } else if (req.method == "DELETE") {
+    const result = await delete_(id);
+    if (result.length === 0) {
+      res.status(HttpStatusCode.Ok).json("Not found");
+    } else {
+      res.status(HttpStatusCode.Ok).json(`Object ${result[0]} deleted`);
+    }
+  } else {
+    res.status(HttpStatusCode.MethodNotAllowed);
+  }
+}
--- src/pages/api/languages/[id].ts
+++ src/pages/api/languages/[id].ts
@@ -1,6 +1,6 @@
+import { resourceEndpoint } from "@/api";
 import getDriver, { query } from "@/db";
 import Language from "@/models/Language";
-import { HttpStatusCode } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const driver = getDriver();
@@ -9,35 +9,23 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Language | string>
 ) {
-  const { id } = req.query;
-  if (req.method === "GET") {
-    const session = driver.session();
-    try {
-      const result = await session.run(
-        "MATCH (lang:Language {id: $id}) RETURN lang;",
-        { id }
-      );
-      if (result.records.length === 0) {
-        res.status(404);
-      }
-      const language: Language = {
-        id: result.records[0].get("lang").properties.id,
-        name: result.records[0].get("lang").properties.name,
-      };
-      res.status(200).json(language);
-    } finally {
-      await session.close();
-    }
-  } else if (req.method == "DELETE") {
-    const language = await query<Language>(
-      driver,
-      `MATCH (lang:Language {id: $id})
-      OPTIONAL MATCH (lang) <-[:IS_IN]- (n)
-      DETACH DELETE lang, n RETURN lang`,
-      { id }
-    );
-    res.status(HttpStatusCode.Ok).json(`Language ${language} deleted`);
-  } else {
-    res.status(HttpStatusCode.MethodNotAllowed);
-  }
+  resourceEndpoint(req, res, getLanguage, deleteLanguage);
+}
+
+async function getLanguage(id: string): Promise<Language[]> {
+  return await query<Language>(
+    driver,
+    "MATCH (lang:Language {id: $id}) RETURN lang;",
+    { id }
+  );
+}
+
+async function deleteLanguage(id: string): Promise<Language[]> {
+  return await query<Language>(
+    driver,
+    `MATCH (lang:Language {id: $id})
+    OPTIONAL MATCH (lang) <-[:IS_IN]- (n)
+    DETACH DELETE lang, n RETURN lang`,
+    { id }
+  );
 }
