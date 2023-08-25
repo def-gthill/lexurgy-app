41da8b925be7e9344a8be2c1332e1bb26fdd5129
Graham Hill <gthill97@gmail.com>
2023-08-25 07:08:50 -0600

    Configure Cypress to try to fix flaky tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     video: false,
+    defaultCommandTimeout: 6000,
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
