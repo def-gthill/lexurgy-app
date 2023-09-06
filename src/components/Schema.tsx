07c3aad2354924218ae5e496cc92a5c66871f527
Graham Hill <gthill97@gmail.com>
2023-09-06 12:59:00 -0600

    Improve schematic editor styling

--- src/components/Schema.tsx
+++ src/components/Schema.tsx
@@ -39,17 +39,29 @@ export class StringSchema implements Schema<string> {
   ): JSX.Element {
     const myOptions = fillDefaults(options);
     const key = myOptions.key;
-    return (
-      <div key={key}>
-        <Label.Root htmlFor={key}>{this.name}</Label.Root>
+    if (myOptions.isRoot) {
+      return (
+        <div key={key}>
+          <Label.Root htmlFor={key}>{this.name}</Label.Root>
+          <input
+            type="text"
+            id={key}
+            value={value as string}
+            onChange={(event) => onChange(event.target.value)}
+          />
+        </div>
+      );
+    } else {
+      return (
         <input
           type="text"
           id={key}
+          style={{ maxWidth: 200 }}
           value={value as string}
           onChange={(event) => onChange(event.target.value)}
         />
-      </div>
-    );
+      );
+    }
   }
 }
 
@@ -81,26 +93,39 @@ export class ObjectSchema<T> implements Schema<T> {
       property,
       schema,
     ]: [Property, Schema<T[Property]>]): JSX.Element {
-      return schema.editor(
-        value[property],
-        (propertyValue) =>
-          onChange({
-            ...value,
-            [property]: propertyValue,
-          }),
-        {
-          isRoot: false,
-          key: `${key}__${property}`,
-        }
+      const childKey = `${key}__${property}`;
+      return (
+        <>
+          <Label.Root htmlFor={childKey}>{schema.name}</Label.Root>
+          {schema.editor(
+            value[property],
+            (propertyValue) =>
+              onChange({
+                ...value,
+                [property]: propertyValue,
+              }),
+            {
+              isRoot: false,
+              key: childKey,
+            }
+          )}
+        </>
       );
     }
 
     return (
       <div id={key} key={key}>
         <h4>{this.name}</h4>
-        {Object.entries(this.properties).map(
-          propertyEditor as (args: [string, unknown]) => JSX.Element
-        )}
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "max-content max-content",
+          }}
+        >
+          {Object.entries(this.properties).map(
+            propertyEditor as (args: [string, unknown]) => JSX.Element
+          )}
+        </div>
       </div>
     );
   }
@@ -130,16 +155,18 @@ export class ArraySchema<T> implements Schema<T[]> {
     return (
       <div id={key} key={key}>
         <h4>{this.name}</h4>
-        {value.map((element, i) =>
-          this.elements.editor(
-            element,
-            (element) => onChange(set(value, i, element)),
-            {
-              isRoot: false,
-              key: `${key}__${i}`,
-            }
-          )
-        )}
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          {value.map((element, i) =>
+            this.elements.editor(
+              element,
+              (element) => onChange(set(value, i, element)),
+              {
+                isRoot: false,
+                key: `${key}__${i}`,
+              }
+            )
+          )}
+        </div>
         <div className="buttons">
           <button onClick={() => onChange([...value, this.elements.empty()])}>
             Add {this.elements.name}
