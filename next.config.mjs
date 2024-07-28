c58b845c8b43397c6cb80f43be693ad08d7bba0c
Graham Hill <gthill97@gmail.com>
2024-07-28 10:12:10 -0600

    Finish the first version of the reference page

--- next.config.mjs
+++ next.config.mjs
@@ -83,7 +83,7 @@ const withMdx = createMDX({
     rehypePlugins: [
       stableHeadingIds,
       wrap,
-      [toc, { headings: ["h2", "h3"], position: "beforebegin" }],
+      [toc, { headings: ["h2", "h3", "h4"], position: "beforebegin" }],
       [rehypeAutolinkHeadings, { behavior: "wrap" }],
     ],
   },
