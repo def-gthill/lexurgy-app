9717a5ac768238329e532fca0e8b622d50457cfe
Graham Hill <gthill97@gmail.com>
2024-09-24 13:48:12 -0600

    Add public access to SC docs (for real this time)

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,6 @@ const publicPages = [
   "/",
   "/sc",
   "/sc/examples",
-  "/sc/docs*",
   "/inflect",
   "/api/services",
   "/api/daily",
@@ -12,11 +11,18 @@ const publicPages = [
   "/logo.png",
 ];
 
+const publicPaths = ["/sc/docs"];
+
 export default withAuth({
   callbacks: {
     authorized({ req, token }) {
       if (publicPages.includes(req.nextUrl.pathname)) {
         return true;
+      }
+      for (const path of publicPaths) {
+        if (req.nextUrl.pathname.startsWith(path)) {
+          return true;
+        }
       }
       if (!token?.email) {
         return false;
