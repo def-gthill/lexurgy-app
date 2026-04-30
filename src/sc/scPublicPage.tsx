2070af14516593c8564501369e06cc41dd3048fa
Graham Hill <gthill97@gmail.com>
2026-04-29 18:07:47 -0600

    Fall back to alert if local storage doesn't work

--- src/sc/scPublicPage.tsx
+++ src/sc/scPublicPage.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
 import PageInfo from "@/components/PageInfo";
-import ScRunner from "@/sc/ScRunner";
+import { loadLocal, saveLocal } from "@/localStorage";
+import Evolution from "@/sc/Evolution";
 import styles from "@/sc/scPublicPage.module.css";
+import ScRunner from "@/sc/ScRunner";
 import { decode } from "js-base64";
 import { useRouter } from "next/router";
-import { loadLocal, saveLocal } from "@/localStorage";
+import { useRef } from "react";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "www.lexurgy.com";
 
@@ -15,12 +17,34 @@ export default function ScPublic() {
   const input = router.query.input;
   let testWords = typeof input === "string" ? decode(input).split("\n") : [""];
 
-  console.log("Sound changes from URL", soundChanges);
-  console.log("Test words from URL", testWords);
   if (!soundChanges && testWords.every((word) => word.trim().length === 0)) {
-    console.log("Loading from local storage!");
     soundChanges = loadLocal("soundChanges") ?? "";
     testWords = loadLocal("testWords")?.split("\n") ?? [""];
+  }
+
+  function onLeave(event: BeforeUnloadEvent) {
+    event.preventDefault();
+    event.returnValue =
+      "You have unsaved changes. Are you sure you want to leave?";
+  }
+
+  const onLeaveActive = useRef(false);
+
+  function checkLeaveListener(evolution: Evolution) {
+    // If local storage isn't working, fall back to
+    // a reminder when navigating away.
+    const outOfSync =
+      loadLocal("soundChanges") !== evolution.soundChanges ||
+      loadLocal("testWords") !== evolution.testWords.join("\n");
+
+    if (outOfSync && !onLeaveActive.current) {
+      onLeaveActive.current = true;
+      window.addEventListener("beforeunload", onLeave);
+    }
+    if (!outOfSync && onLeaveActive.current) {
+      onLeaveActive.current = false;
+      window.removeEventListener("beforeunload", onLeave);
+    }
   }
 
   return (
@@ -41,6 +65,7 @@ export default function ScPublic() {
             onUpdate={(evolution) => {
               saveLocal("soundChanges", evolution.soundChanges);
               saveLocal("testWords", evolution.testWords.join("\n"));
+              checkLeaveListener(evolution);
             }}
           />
         </main>
